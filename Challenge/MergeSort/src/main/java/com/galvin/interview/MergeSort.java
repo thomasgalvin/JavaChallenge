0629636531e41aa021fbc7f48e0832aa2da330a6
Thomas Galvin <tgalvin@caci.com>
2017-08-01 13:00:47 -0400

    Added MergeSort challenge

---
+++ Challenge/MergeSort/src/main/java/com/galvin/interview/MergeSort.java
@@ -0,0 +1,25 @@
+package com.galvin.interview;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * This class implements the MergeSort algorithm.
+ * 
+ * The MergeSort algorithm can be defined as:
+ * 
+ * Given a List of values:
+ * 1. Divide the unsorted list into n sublists, each containing 1 element (a list of 1 element is considered sorted).
+ * 2. Repeatedly merge sublists to produce new sorted sublists until there is only 1 sublist remaining. This will be the sorted list.
+ * 
+ * Implement the MergeSort algorithm below. You may assume all values will be
+ * non-null.
+ * 
+ * For more information:
+ * https://en.wikipedia.org/wiki/Merge_sort
+ */
+public class MergeSort {
+    public void sort( List<Integer> target ) {
+        throw new UnsupportedOperationException( "TODO: Not implemented yet" );
+    }
+}
---
+++ Challenge/MergeSort/src/test/java/com/galvin/interview/MergeSortTest.java
@@ -0,0 +1,105 @@
+package com.galvin.interview;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import org.junit.Assert;
+import org.junit.Test;
+
+public class MergeSortTest
+{
+    @Test
+    public void testMergesortPresorted() throws Exception {
+        List<Integer> target = getTestData();
+        doMergeSort( target );
+    }
+    
+    @Test
+    public void testMergesortBackwards() throws Exception {
+        List<Integer> target = getTestData();
+        Collections.reverse( target );
+        doMergeSort( target );
+    }
+    
+    @Test
+    public void testMergesortShuffle() throws Exception {
+        List<Integer> target = getTestData();
+        Collections.shuffle( target );
+        doMergeSort( target );
+    }
+    
+    @Test
+    public void testMergesortDuplicates() throws Exception {
+        List<Integer> target = new ArrayList();
+        target.addAll( getTestData() );
+        target.addAll( getTestData() );
+        target.addAll( getTestData() );
+        target.addAll( getTestData() );
+        target.addAll( getTestData() );
+        Collections.shuffle( target );
+        doMergeSort( target );
+    }
+    
+    @Test
+    public void testMergesortRandom() throws Exception {
+        for( int i = 0; i < 1000; i++ ){
+            doRandomMergeSort();
+        }
+    }
+    
+    private List<Integer> getTestData(){
+        List<Integer> target = new ArrayList();
+        target.add( -10 );
+        target.add( -9 );
+        target.add( -8 );
+        target.add( -7 );
+        target.add( -6 );
+        target.add( -5 );
+        target.add( -4 );
+        target.add( -3 );
+        target.add( -2 );
+        target.add( -1 );
+        target.add( 0 );
+        target.add( 1 );
+        target.add( 2 );
+        target.add( 3 );
+        target.add( 4 );
+        target.add( 5 );
+        target.add( 6 );
+        target.add( 7 );
+        target.add( 8 );
+        target.add( 9 );
+        target.add( 10 );
+        return target;
+    }
+    
+    private void doMergeSort(List<Integer> target ) throws Exception {
+        List<Integer> expected = new ArrayList();
+        expected.addAll( target );
+        Collections.sort( expected );
+        
+        MergeSort sort = new MergeSort();
+        sort.sort( target );
+        
+        Assert.assertEquals( "MergeSort failed", expected, target );
+    }
+    
+    private void doRandomMergeSort() throws Exception {
+        List<Integer> expected = new ArrayList();
+        for( int i = 0; i < 5; i++ ) {
+            for( int j = 0; j < 500; j++ ) {
+                expected.add( j );
+            }
+        }
+        Collections.sort( expected );
+        
+        List<Integer> target = new ArrayList();
+        target.addAll( expected );
+        Collections.shuffle( target );
+        
+        MergeSort sort = new MergeSort();
+        sort.sort( target );
+        
+        Assert.assertEquals( "MergeSort failed", expected, target );
+    }
+}
